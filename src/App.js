4895473096cd7fee38f8d2b58abd364e07f0ae30
이주영 <stylejy@gmail.com>
2017-12-24 17:19:07 +0000

    CSS customised.
    CSS settings provided from Codeacademy didnt work well as it was supposed to be. So some codes were changed.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
